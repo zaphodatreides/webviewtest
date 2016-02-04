93c9e501c37093ef893a62f7cb72d4ef8e80b4d6
zaphodatreides <v.atreides2010@gmail.com>
2016-02-04 21:17:36 +0100

    2 webviews

--- gen/com/basic/R.java
+++ gen/com/basic/R.java
@@ -24,12 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int back=0x7f080003;
         public static final int run=0x7f080001;
         public static final int save=0x7f080002;
         public static final int url=0x7f080000;
         public static final int webview=0x7f080004;
+        public static final int webview2=0x7f080005;
     }
     public static final class layout {
         public static final int activity_web_view_example=0x7f030000;
--- src/com/basic/WebViewExample.java
+++ src/com/basic/WebViewExample.java
@@ -9,11 +9,41 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.util.Log;
+import java.io.RandomAccessFile; 
+
 
 public class WebViewExample extends Activity{ 
 
     WebView webView;
+    WebView webView2;
     EditText text;
+    int scrollx,scrolly;
+    String mub = "file:///sdcard/beautifulsoup/";
+    String mue = "/test3.html";
+    String mainurl;
+    String secondaryurl = null;
+    
+    @Override
+    
+    public void onBackPressed(){
+    	Log.e ("basic","back pressed");
+    	
+    if (secondaryurl != null){
+    	Log.e ("basic",webView2.getUrl());
+    	webView2.setVisibility(View.INVISIBLE );
+    	webView.setVisibility(View.VISIBLE);
+    	secondaryurl = null;
+    	Toast.makeText(getApplicationContext(), " Press Back again to Exit ", Toast.LENGTH_SHORT).show();
+
+   	}
+    else {
+    	super.onBackPressed();
+    	Log.e ("basic","string is null");
+    }
+    }
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -21,6 +51,7 @@ public class WebViewExample extends Activity{
         setContentView(R.layout.activity_web_view_example);
 
         webView = (WebView) findViewById(R.id.webview);
+        webView2 = (WebView) findViewById(R.id.webview2);
 
         Button button = (Button) findViewById (R.id.run);
         button.setOnClickListener(new OnClickListener() {           
@@ -33,7 +64,7 @@ public class WebViewExample extends Activity{
         save.setOnClickListener(new OnClickListener() {           
             @Override
             public void onClick(View v) {
-                saveLink();             
+                saveLink();   
             }
         });
         
@@ -41,41 +72,82 @@ public class WebViewExample extends Activity{
         back.setOnClickListener(new OnClickListener() {           
             @Override
             public void onClick(View v) {
-                discardLink();             
+                secondaryurl=null;
+                webView2.setVisibility(View.INVISIBLE);
+                webView.setVisibility(View.VISIBLE );
             }
         });
+        
         text = (EditText) findViewById(R.id.url);
         text.setMaxLines(1);
     }
 
+ /*   private void restoreview(){
+    	
+    	webView.loadUrl(mainurl);
+   	}*/
     private void gotoPage(){
 
-        String url = text.getText().toString();
+        mainurl = mub+text.getText().toString()+mue;
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setBuiltInZoomControls(true);
 
         webView.setWebViewClient(new Callback());  //HERE IS THE MAIN CHANGE
-        webView.loadUrl(url);
+        Log.e ("basic",mainurl);
+        webView.loadUrl(mainurl);
 
     }
     
     private void saveLink(){
-    	text.setText(webView.getUrl());
+
+    	try {
+    	  RandomAccessFile outputStream = new  RandomAccessFile("/sdcard/links.txt", "rw");
+    	  outputStream.seek(outputStream.length());
+    	  outputStream.write(secondaryurl.getBytes());
+    	  outputStream.write("\n".getBytes());
+    	  outputStream.close();
+    	} catch (Exception e) {
+    	  e.printStackTrace();
+    	}
     	
+    	//text.setText(webView.getUrl());
+    	webView2.setVisibility(View.INVISIBLE);
+    	webView.setVisibility(View.VISIBLE);
+    	secondaryurl = null;
     	
+  	   	
     }
     
-    private void discardLink(){
+/*    private void discardLink(){
+    	webView2.setVisibility(View.GONE);
+    	webView.setVisibility(View.VISIBLE);
     	
-    }
-
+    }*/
+ 
     private class Callback extends WebViewClient{  //HERE IS THE MAIN CHANGE. 
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        	//scrollx=view.getScrollX();
+        	//scrolly=view.getScrollY();
+        	//Log.e("basic",String.format("getscrollx&y called returned values %d %d ",scrollx,scrolly));
+        	secondaryurl=url;
+        	webView2.loadUrl(secondaryurl);
+        	webView.setVisibility(View.INVISIBLE);
+        	webView2.setVisibility(view.VISIBLE);
             return (false);
         }
+    /*    public void onPageFinished(WebView view, String url) {
+        	Log.e ("basic","OnPageFinished called");
+           if (url.equals(mainurl)) {
+        	   //while(webView.getProgress()!=100){
+        	   //}
+    	       //webView.scrollTo(scrollx,scrolly);
+        	   //Log.e("basic","scrolled to x,y");
+        	   
+           }
+        }*/
 
     }
 
